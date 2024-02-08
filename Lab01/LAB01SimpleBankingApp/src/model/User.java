ff81ffd39cb3fd2268e911c5deae743f6fa82a65
erinmcc00l <159448830+erinmcc00l@users.noreply.github.com>
2024-02-08 15:10:25 +0000

    Add files via upload

--- Lab01/LAB01SimpleBankingApp/src/model/User.java
+++ Lab01/LAB01SimpleBankingApp/src/model/User.java
@@ -1,14 +1,13 @@
 package model;
 
 public class User {
-	private String username;
+	private String username; // email address
 	private String password;
 	private String first_name;
 	private String last_name;
 	private String mobile_number;
-	
-	
-	// Constructor
+
+	// Constructor for the User class
 	public User(String username, String password, String first_name, String last_name, String mobile_number) {
 		super();
 		this.username = username;
@@ -17,44 +16,54 @@ public class User {
 		this.last_name = last_name;
 		this.mobile_number = mobile_number;
 	}
-	
-	// Getters and Setters 
+
+	/*
+	 * Getters and Setters for user fields: username, password, first name, last
+	 * name, and mobile number.
+	 */
 	public String getUsername() {
 		return username;
 	}
+
 	public void setUsername(String username) {
 		this.username = username;
 	}
+
 	public String getPassword() {
 		return password;
 	}
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
+
 	public String getFirst_name() {
 		return first_name;
 	}
+
 	public void setFirst_name(String first_name) {
 		this.first_name = first_name;
 	}
+
 	public String getLast_name() {
 		return last_name;
 	}
+
 	public void setLast_name(String last_name) {
 		this.last_name = last_name;
 	}
+
 	public String getMobile_number() {
 		return mobile_number;
 	}
+
 	public void setMobile_number(String mobile_number) {
 		this.mobile_number = mobile_number;
 	}
 
 	@Override
 	public String toString() {
-		return String.format("%-25s| %-15s| %-15s| %-15s| %-15s", 
-				username , password, first_name, last_name, mobile_number);
+		return String.format("%-25s| %-15s| %-15s| %-15s| %-15s", username, password, first_name, last_name,
+				mobile_number);
 	}
-	
-	
 }
