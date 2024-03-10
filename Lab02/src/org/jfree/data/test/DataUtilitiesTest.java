5b897a5d01e9234612abc27714f4bed6653115bb
noconnor07 <159444730+noconnor07@users.noreply.github.com>
2024-03-10 00:07:15 +0000

    Add files via upload

---
+++ Lab02/src/org/jfree/data/test/DataUtilitiesTest.java
@@ -0,0 +1,51 @@
+package org.jfree.data.test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.security.InvalidParameterException;
+
+import org.jfree.data.DataUtilities;
+import org.jfree.data.DefaultKeyedValues2D;
+import org.jfree.data.Values2D;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class DataUtilitiesTest  {
+
+	private Values2D values2D;
+	
+	@BeforeEach
+	void setUp(){
+		DefaultKeyedValues2D testValues = new DefaultKeyedValues2D();
+		values2D = testValues;
+		testValues.addValue(1, 0, 0);
+		testValues.addValue(4, 1, 0);
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+		values2D = null; 
+	}
+
+	@Test
+	public void testValidDataAndColumnColumnTotal() {
+		assertEquals("Wrong sum returned. It should be 5.0", 
+				5.0, DataUtilities.calculateColumnTotal(values2D, 0), 0.0000001d);
+
+	}
+	
+	@Test
+	public void testNullDataColumnTotal() {
+		try {
+			DataUtilities.calculateColumnTotal(null, 0);
+			fail("No exception thrown");
+		} catch (Exception e) {
+			assertTrue("Incorrect exception type thrown", 
+					e.getClass().equals(InvalidParameterException.class));
+		}
+	}
+
+}
