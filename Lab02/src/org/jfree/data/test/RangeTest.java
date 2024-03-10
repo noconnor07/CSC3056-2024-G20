77a1f26c4262d7ec54c777350d369ceea3a158d3
noconnor07 <159444730+noconnor07@users.noreply.github.com>
2024-03-10 20:47:10 +0000

    Add files via upload

--- Lab02/src/org/jfree/data/test/RangeTest.java
+++ Lab02/src/org/jfree/data/test/RangeTest.java
@@ -11,11 +11,11 @@ import org.jfree.data.Range;
 
 public class RangeTest{
 	
-	private Range rangeObjectUnderTestRange;
+//	private Range rangeObjectUnderTestRange;
 
 	@Before
 	public void setUp() throws Exception {
-		rangeObjectUnderTestRange = new Range(-1,1);
+//		rangeObjectUnderTestRange = new Range(-1,1);
 	}
 
 	@After
@@ -24,7 +24,8 @@ public class RangeTest{
 
 	
 	// Test Cases for getCentralValue 
-	
+		
+		// TC1
 	 	@Test
 	    public void testGetCentralValueWithPositiveRange() {
 	        Range range = new Range(1, 3);
@@ -32,6 +33,7 @@ public class RangeTest{
 	        assertEquals("The centeral value of 1 and 3 should be 2", expected, range.getCentralValue(), 0.000000001d);
 	    }
 	 
+	 	// TC2
 	 	@Test
 	 	public void testGetCentralValueWithPositiveRangeShouldFail() {
 		    Range range = new Range(1, 3);
@@ -39,6 +41,7 @@ public class RangeTest{
 		    assertNotEquals("The central value of 1 and 3 should not be 1.5", unexpected, range.getCentralValue(), 0.000000001d);
 		}
 
+	 	// TC3
 	    @Test
 	    public void testGetCentralValueWithNegativeRange() {
 	        Range range = new Range(-3, -1);
@@ -46,6 +49,7 @@ public class RangeTest{
 	        assertEquals("The centeral value of -3 and -1 should be -2", expected, range.getCentralValue(), 0.000000001d);
 	    }
 	    
+	    // TC4
 	    @Test
 	    public void testGetCentralValueWithNegativeRangeShouldFail() {
 	        Range range = new Range(-3, -1);
@@ -53,6 +57,7 @@ public class RangeTest{
 	        assertNotEquals("The central value of -3 and -1 should not be -1.5", unexpected, range.getCentralValue(), 0.000000001d);
 	    }
 
+	    // TC5
 	    @Test
 	    public void testGetCentralValueWithMixedRange() {
 	        Range range = new Range(-1, 1);
@@ -60,6 +65,7 @@ public class RangeTest{
 	        assertEquals("The centeral value of -1 and 1 should be 0", expected, range.getCentralValue(), 0.000000001d);
 	    }
 	    
+	    // TC6
 	    @Test
 	    public void testGetCentralValueWithMixedRangeShouldFail() {
 	        Range range = new Range(-1, 1);
@@ -69,6 +75,7 @@ public class RangeTest{
 	    
 	 // Test Cases for getLowerBound
 	    
+	    // TC1
 	    @Test
 	    public void testGetLowerBoundWithPositiveRange() {
 	        Range range = new Range(1, 5);
@@ -76,6 +83,7 @@ public class RangeTest{
 	        assertEquals("The lower bound value of the range should be 1", expectedLower, range.getLowerBound(), 0.000000001d);
 	    }
 
+	    // TC2
 	    @Test
 	    public void testGetLowerBoundWithPositiveRangeShouldFail() {
 	        Range range = new Range(1, 5);
@@ -83,6 +91,7 @@ public class RangeTest{
 	        assertNotEquals("The lower bound value of the range should not be 2", unexpectedLower, range.getLowerBound(), 0.000000001d);
 	    }
 	
+	    // TC3
 	    @Test
 	    public void testGetLowerBoundWithNegativeRange() {
 	        Range range = new Range(-5, -1);
@@ -90,6 +99,7 @@ public class RangeTest{
 	        assertEquals("The lower bound value of the range should be -5", expectedLower, range.getLowerBound(), 0.000000001d);
 	    }
 
+	    // TC4
 	    @Test
 	    public void testGetLowerBoundWithNegativeRangeShouldFail() {
 	        Range range = new Range(-5, -1);
@@ -97,6 +107,7 @@ public class RangeTest{
 	        assertNotEquals("The lower bound value of the range should not be -4", unexpectedLower, range.getLowerBound(), 0.000000001d);
 	    }
 	    
+	    // TC5
 	    @Test
 	    public void testGetLowerBoundWithMixedRange() {
 	        Range range = new Range(-1, 1);
@@ -104,6 +115,7 @@ public class RangeTest{
 	        assertEquals("The lower bound value of -1 and 1 should be -1", expectedLowerBound, range.getLowerBound(), 0.000000001d);
 	    }
 
+	    // TC6
 	    @Test
 	    public void testGetLowerBoundWithMixedRangeShouldFail() {
 	        Range range = new Range(-1, 1);
@@ -114,15 +126,16 @@ public class RangeTest{
 
 	 // Test Cases for getUpperBound 
 	    
-		
+	    // TC1
 		@Test
 		public void testGetUpperBoundWithPositiveRange() {
 		    Range range = new Range(1, 5);
 		    double expectedUpper = 5;
-		    System.out.println("testGetUpperBoundWithPositiveRange result " + range.getUpperBound());
+		   // System.out.println("testGetUpperBoundWithPositiveRange result " + range.getUpperBound());
 		    assertEquals("The upper bound value of the range should be 5", expectedUpper, range.getUpperBound(), 0.000000001d);
 		}
 		
+		// TC2
 		@Test
 		public void testGetUpperBoundWithPositiveRangeShouldFail() {
 		    Range range = new Range(1, 5);
@@ -130,14 +143,16 @@ public class RangeTest{
 		    assertNotEquals("The upper bound value  of the range should not be 4", unexpectedUpper, range.getUpperBound(), 0.000000001d);
 		}
 		
+		// TC3
 		@Test
 		public void testGetUpperBoundWithNegativeRange() {
 		    Range range = new Range(-5, -1);
 		    double expectedUpper = -1.0;
-		    System.out.println("testGetUpperBoundWithPositiveRange result " + range.getUpperBound());
+		   // System.out.println("testGetUpperBoundWithPositiveRange result " + range.getUpperBound());
 		    assertEquals("The upper bound value of the range should be -1", expectedUpper, range.getUpperBound(), 0.000000001d);
 		}
 
+		// TC4
 		@Test
 		public void testGetUpperBoundWithNegativeRangeShouldFail() {
 		    Range range = new Range(-5, -1);
@@ -145,14 +160,16 @@ public class RangeTest{
 		    assertNotEquals("The upper bound value of the range should not be -2", unexpectedUpper, range.getUpperBound(), 0.000000001d);
 		}
 
+		// TC5
 	    @Test
 	    public void testGetUpperBoundWithMixedRange() {
 	        Range range = new Range(-1, 1);
 	        double expectedUpperBound = 1;
-	        System.out.println("testGetUpperBoundWithMixedRange result " + range.getUpperBound());
+	       // System.out.println("testGetUpperBoundWithMixedRange result " + range.getUpperBound());
 	        assertEquals("The upper bound value of -1 and 1 should be 1", expectedUpperBound, range.getUpperBound(), 0.000000001d);
 	    }
 
+	    // TC6
 	    @Test
 	    public void testGetUpperBoundWithMixedRangeShouldFail() {
 	        Range range = new Range(-1, 1);
@@ -161,6 +178,8 @@ public class RangeTest{
 	    }
 	    
 	 // Test Cases for Intersects 
+	    
+	    // TC1
 	    @Test
 	    public void testIntersectsPositiveRange() {
 	        Range baseRange = new Range(1, 5);
@@ -169,6 +188,7 @@ public class RangeTest{
 	            baseRange.intersects(intersectingRange.getLowerBound(), intersectingRange.getUpperBound()));
 	    }
 
+	    // TC2
 	    @Test
 	    public void testDoesNotIntersectPositiveRange() {
 	        Range baseRange = new Range(1, 5);
@@ -177,6 +197,7 @@ public class RangeTest{
 	            baseRange.intersects(nonIntersectingRange.getLowerBound(), nonIntersectingRange.getUpperBound()));
 	    }
 	    
+	    // TC3
 	    @Test
 	    public void testIntersectsNegativeRange() {
 	        Range baseRange = new Range(-5, -1);
@@ -185,15 +206,17 @@ public class RangeTest{
 	            baseRange.intersects(intersectingRange.getLowerBound(), intersectingRange.getUpperBound()));
 	    }
 
+	    // TC4
 	    @Test
 	    public void testDoesNotIntersectNegativeRange() {
 	        Range baseRange = new Range(-5, -1);
 	        Range nonIntersectingRange = new Range(-10, -6);
-	        System.out.println("testDoesNotIntersectNegativeRange result " + nonIntersectingRange.getLowerBound() + nonIntersectingRange.getUpperBound());
+	       //  System.out.println("testDoesNotIntersectNegativeRange result " + nonIntersectingRange.getLowerBound() + nonIntersectingRange.getUpperBound());
 	        Assert.assertFalse("The ranges should not intersect", 
 	            baseRange.intersects(nonIntersectingRange.getLowerBound(), nonIntersectingRange.getUpperBound()));
 	    }
 	    
+	    // TC5
 	    @Test
 	    public void testIntersectsMixedRange() {
 	        Range baseRange = new Range(-3, 3);
@@ -202,6 +225,7 @@ public class RangeTest{
 	            baseRange.intersects(intersectingRange.getLowerBound(), intersectingRange.getUpperBound()));
 	    }
 
+	    // TC6
 	    @Test
 	    public void testDoesNotIntersectMixedRange() {
 	        Range baseRange = new Range(-3, 3);
@@ -212,6 +236,7 @@ public class RangeTest{
 	    
 	 // Test Cases for Expand to Include 
 	    
+	    // TC1
 	    @Test
 	    public void testExpandToIncludePositiveRange() {
 	        Range range = new Range(1, 5);
@@ -221,6 +246,7 @@ public class RangeTest{
 	        Assert.assertEquals("The lower bound should remain the same", 1, expandedRange.getLowerBound(), 0.000000001d);
 	    }
 
+	    // TC2
 	    @Test
 	    public void testExpandToIncludeNegativeRange() {
 	        Range range = new Range(-5, -1);
@@ -230,6 +256,7 @@ public class RangeTest{
 	        Assert.assertEquals("The upper bound should remain the same", -1, expandedRange.getUpperBound(), 0.000000001d);
 	    }
 
+	    // TC3
 	    @Test
 	    public void testExpandToIncludeMixedRange() {
 	        Range range = new Range(-3, 3);
@@ -239,6 +266,7 @@ public class RangeTest{
 	        Assert.assertEquals("The lower bound should remain the same", -3, expandedRange.getLowerBound(), 0.000000001d);
 	    }
 	    
+	    // TC4
 	    @Test
 	    public void testExpandToIncludeValueAlreadyInPositiveRange() {
 	        Range range = new Range(1, 5);
@@ -246,6 +274,26 @@ public class RangeTest{
 	        Range expandedRange = Range.expandToInclude(range, valueToInclude);
 	        Assert.assertEquals("The upper bound should remain the same when including 3", 5, expandedRange.getUpperBound(), 0.000000001d);
 	        Assert.assertEquals("The lower bound should remain the same when including 3", 1, expandedRange.getLowerBound(), 0.000000001d);
+	    }
+	    
+	    // TC5
+	    @Test
+	    public void testExpandToIncludeValueAlreadyInNegativeRange() {
+	        Range range = new Range(-5, -1);
+	        double valueToInclude = -3;
+	        Range expandedRange = Range.expandToInclude(range, valueToInclude);
+	        Assert.assertEquals("The upper bound should remain the same when including -3", -1, expandedRange.getUpperBound(), 0.000000001d);
+	        Assert.assertEquals("The lower bound should remain the same when including -3", -5, expandedRange.getLowerBound(), 0.000000001d);
+	    }
+
+	    // TC6
+	    @Test
+	    public void testExpandToIncludeValueAlreadyInMixedRange() {
+	        Range range = new Range(-3, 3);
+	        double valueToInclude = 0;
+	        Range expandedRange = Range.expandToInclude(range, valueToInclude);
+	        Assert.assertEquals("The upper bound should remain the same when including 0", 3, expandedRange.getUpperBound(), 0.000000001d);
+	        Assert.assertEquals("The lower bound should remain the same when including 0", -3, expandedRange.getLowerBound(), 0.000000001d);
 	    }
 	    
 	    
